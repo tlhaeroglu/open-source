ba6ac5c590b4074d1ae2948f14207cc60a17397e
tlhaeroglu <talha44el@gmail.com>
2022-11-03 19:23:31 +0300

    rest-api

--- README.md
+++
@@ -1,5 +0,0 @@
-# open-source
-
-### Simple Get Request Web Search Api
-
-Get query with news -> `output.json`
--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-const express = require("express");
-const axios = require("axios");
-
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const notesRouter = require('./routes/notes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors({
+    origin: "*",
+}))
+require('dotenv/config');
+const conectionString = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.dt3dr.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 
-const options = {
-    method: 'GET',
-    url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI',
-    params: {
-      q: 'elon musk',
-      pageNumber: '1',
-      pageSize: '10',
-      autoCorrect: 'true',
-      fromPublishedDate: 'null',
-      toPublishedDate: 'null'
-    },
-    headers: {
-      'X-RapidAPI-Key': 'ccea9077c6msh17df179eab102a9p10281bjsnae4e3051eb5f',
-      'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+mongoose.connect(conectionString, (e)=>{
+    if(e){
+        console.log(e);
+    }else{
+        console.log('Connected to database');
     }
-};
-
-app.get("/", (req,res) =>{
-    axios.request(options).then(function (response) {
-        res.json(response.data);
-    }).catch(function (error) {
-        res.send(error);
-    });
 });
 
-app.listen(4500, () =>{
-    console.log("http://127.0.0.1:4500");
+app.use('/notes', notesRouter);
+app.use(bodyParser.json());
+//app.use(cors());
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
 });
